149210623283bc14f6964ecd052bf6bb84f94e95
DelinLi <delin.bio@gmail.com>
2015-02-08 11:48:11 +0800

    Extract.Sequence.pl V1.01

--- BLAST/NRannotation/Extract.Sequence.pl
+++ BLAST/NRannotation/Extract.Sequence.pl
@@ -1,7 +1,8 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl 
 ##By Delin LI  delin.bio@gamil.com
 ###Extract Gene/mRNA/CDS sequence from reference genome to fasta format
 ##V1.00 only work on specific chromosome/scaffold for now
+##V1.01 could extract sequence from any chromosomes, the input gene table should be with 4 columns
 
 use strict;
 use warnings;
@@ -19,28 +20,30 @@ my $result = &GetOptions("reference|f=s{1}" => \$reference,
 
 #Ask for parameters
 unless ($result && defined($reference) && defined($genes) && defined($output)) {
-	print STDERR sprintf("perl %s --reference|f <reference fasta> --genes|g <gene table with three columns> --output <out fasta>\n", $0);
+	print STDERR sprintf("\n");
+	print STDERR sprintf("perl %s --reference|-f <reference fasta> --genes|-g <gene table with four columns> --output|-o <out fasta>\n", $0);
+	print STDERR sprintf("\n");
+	print STDERR sprintf("\t--genes|-g The four columns of gene table: Chr, ID, StartPosition, EndPosition\n\n");
 	exit();
 } 
 
-
-my $seq;
+my (%genome,$seq,$name);
 my $fh = new FileHandle();
 open ($fh, $reference) or die("Cannot open fasta file\n");
-my $target=0;
 while (<$fh>) {
 	chomp;
 	if (length($_) != 0) {
-		if ($_ =~ m/^>DAUCA_BGI_Solexa_pseudochr_2_1_Chr_08/){
-			$target=1;
-		}elsif($_ =~ m/^>/){ $target=0}
-		else{
-			if($target==1){
-				$seq .= $_;
-			} # End of else statement
-		}
-	} # end of if statement
-} # end of while loop
+		if ($_ =~ m/^>(\S+)/){
+            if(defined($name)){
+                $genome{$name}=$seq;
+            }
+        $name=$1;
+        $seq="";
+        }else{
+            $seq.=$_;
+        }
+    }#end of if
+}#end of while
 close ($fh);
 
 my $ofh = new FileHandle();
@@ -48,9 +51,14 @@ open (IN,$genes) or die("Cannot open gene table\n");
 open ($ofh,">$output") or die("Cannot create output fasta file\n");
 while(<IN>){
 	my@line=split(/\t/,$_);
-	my$sequence=substr($seq, $line[1] - 1, $line[2] - $line[1] + 1);
-	print $ofh ">$line[0]\n";
-	print $ofh "$sequence\n";
-}
+	if(!defined($genome{$line[0]})){
+		print STDERR "Line without recognized chromosome name, please ignore this if this is a headline:\n";
+		print STDERR "$_";
+	}else{
+		my$sequence=substr($genome{$line[0]}, $line[2] - 1, $line[3] - $line[2] + 1);
+		print $ofh ">$line[1]\n";
+		print $ofh "$sequence\n";
+	}
+}#end of while loop
 close IN;
 close $ofh;
